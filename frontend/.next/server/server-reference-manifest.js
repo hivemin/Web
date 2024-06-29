e7b0ef31615b969923b99ec5bf9649ecba552614
hivemin <oscarsdl13@gmail.com>
2024-06-29 23:32:31 +0200

    recuperacion backend

--- frontend/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"nq6bTxhD6BKikiuWoSqi61gjlsHWDYG/+YeKldYVIBs=\"\n}"
--- frontend/components/usuario.js
+++
@@ -1,8 +0,0 @@
-export default function Usuario({usuario}) {
-    return (
-        <li className="list-group-item">
-            <p>Email: {usuario.email}</p>
-            <p>Ciudad: {usuario.ciudad}</p>
-        </li>
-    )
-}
